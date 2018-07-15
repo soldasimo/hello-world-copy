9e98823a7e0845972c7134c2b4979b29b662a482
Simone Soldateschi <simone.soldateschi@gmail.com>
2018-07-15 17:21:55 +0100

    first commit

---
+++ src/main/java/us/fetchr/sample/WebController.java
@@ -0,0 +1,15 @@
+package us.fetchr.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+
+}
